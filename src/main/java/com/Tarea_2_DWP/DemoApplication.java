dfdfbb4175c4a9b4d3cca9afe6d6fa760bd46e33
andreyar78 <andreyar78aguero@outlook.com>
2022-06-10 06:46:02 -0600

    Creacion de proyecto

---
+++ src/main/java/com/Tarea_2_DWP/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea_2_DWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tarea_2_DWP/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea_2_DWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
